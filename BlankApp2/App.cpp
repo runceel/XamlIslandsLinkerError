e265f1bf5a9b93f336b668101b0e4db467f31f77
Kazuki Ota <kaota@microsoft.com>
2019-09-25 17:44:37 +0900

    Add project files.

---
+++ BlankApp2/App.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+
+#include "App.h"
+#include "App.g.cpp"
+
+namespace winrt::BlankApp2::implementation
+{
+	App::App()
+	{
+		Initialize();
+		AddRef();
+		m_inner.as<::IUnknown>()->Release();
+	}
+	App::~App()
+	{
+		Close();
+	}
+}
---
+++ BlankApp2/App.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "App.g.h"
+#include "App.xaml.g.h"
+#include "App.base.h"
+
+namespace winrt::BlankApp2::implementation
+{
+    struct App : AppT2<App>
+    {
+        App();
+		~App();
+    };
+}
+
+namespace winrt::BlankApp2::factory_implementation
+{
+	class App : public AppT<App, implementation::App>
+	{
+	};
+}
